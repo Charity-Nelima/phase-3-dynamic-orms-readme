a24ecde040cc9a2ef7ad1974aeac6ad9b616b71c
charity <wa.nelimacharity@gmail.com>
2022-11-02 22:27:51 +0300

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
 class Song
 
 
   def self.table_name
     self.to_s.downcase.pluralize
   end
+
 
   def self.column_names
     DB[:conn].results_as_hash = true
